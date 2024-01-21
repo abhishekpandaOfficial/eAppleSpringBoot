1d03d8fa8f8b9315abc1c23df7af396d0c6c29a8
abhishekpandaOfficial <official.abhishekpanda@gmail.com>
2024-01-21 12:51:49 +0530

    Adding test containers packages and unit tests

--- product-service/src/test/java/com/morningmonk/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/morningmonk/productservice/ProductServiceApplicationTests.java
@@ -1,13 +1,28 @@
 package com.morningmonk.productservice;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
-class ProductServiceApplicationTests {
+@Testcontainers
+public class ProductServiceApplicationTests
+{
+  @Container
+  static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
+  @DynamicPropertySource
+  static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry)
+      {
+          dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+      }
+  @Test
+  void contextLoads(){
+  }
 
-	@Test
-	void contextLoads() {
-	}
 
 }
